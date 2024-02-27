f4041221d8f2a096624d2eaa19dfc1374b143d2a
himanshudas13 <das13himanshu@gmail.com>
2024-02-27 19:35:21 +0530

    modified

--- src/Login.js
+++ src/Login.js
@@ -1,32 +1,56 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@mui/material";
-import{auth,provider} from "./firebase";
+import { auth, provider} from "./firebase";
+import db from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { actionTypes } from "./reducer";
 
 function Login() {
-    const[state,dispatch]=useStateValue();
-const signIn = () =>{
+  const [state, dispatch] = useStateValue();
+
+  const signIn = () => {
     auth
-    .signInWithPopup(provider)
-    .then((result)=>{console.log(result);
-    dispatch({type:actionTypes.SET_USER,
-    user:result.user,
-    })})
-    .catch((error)=>{alert(error.message);});
-}
+      .signInWithPopup(provider)
+      .then(async (result) => {
+        console.log(result);
 
-    return <div className="login">
-        <div className='Login__container'>
-            <img src="https://www.sketchappsources.com/resources/source-image/new-slack-logo-nicolas-ciotti.jpg" alt="SLACK" />
-         <h5>Sign in to SLACK</h5>
-         
-         <Button onClick={signIn}>Sign in with Google</Button>
-        
-        </div>
+        // Check if the user exists in the "users" collection
+        const userRef = db.collection('users').doc(result.user.uid);
+        const userDoc = await userRef.get();
 
+        if (!userDoc.exists) {
+          // If the user does not exist, create a new document in the "users" collection
+          await userRef.set({
+            displayName: result.user.displayName,
+            email: result.user.email,
+            channels:[],
+            // Add other user details as needed
+          });
+        }
+
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div className="Login__container">
+        <img
+          src="https://www.sketchappsources.com/resources/source-image/new-slack-logo-nicolas-ciotti.jpg"
+          alt="SLACK"
+        />
+        <h5>Sign in to SLACK</h5>
+        <Button onClick={signIn}>Sign in with Google</Button>
+      </div>
     </div>
+  );
 }
 
 export default Login;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import './Sidebar.css'
+import React, { useState, useEffect } from 'react';
+import './Sidebar.css';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
@@ -15,51 +15,80 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import db from "./firebase";
 import { useStateValue } from './StateProvider';
+import { getUserChannels } from './user_channels';
+
 
 function Sidebar() {
-  const [channels, setChannels] = useState([]);
- const[{user}]=useStateValue();
-  useEffect(() => {
-    db.collection('rooms').onSnapshot((snapshot) => {
-      setChannels(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          name: doc.data().name,
-        }))
-      );
-    });
-  }, []);
-    return (
-        <div className='sidebar'>
-          <div className='sidebar_header'>
-            <div className='sidebar_info'>
-                <h2>User_Name</h2>
-                <h3>
-                   <FiberManualRecordIcon/>
-                    {user?.displayName}
-                </h3>
-            </div>
-            <CreateIcon/>
-            </div>
-            <SidebarOption Icon={InsertCommentIcon} title='Threads'/>
-       <SidebarOption Icon = {InboxIcon} title='Mentions & Reactions'/>
-       <SidebarOption Icon={DraftsIcon} title='Saved Items'/>
-       <SidebarOption Icon={BookmarkBorderIcon} title='Channel Browser'/>
-       <SidebarOption Icon={PeopleAltIcon} title='People and User Groups'/>
-       <SidebarOption Icon={AppsIcon} title='Apps'/>
-       <SidebarOption Icon={FileCopyIcon} title='File Browser'/>
-       <SidebarOption Icon={ExpandLessIcon} title='Show less'/>
-       <hr/>
-       <SidebarOption Icon={ExpandMoreIcon} title='Channels'/>
-       <hr/>
-       <SidebarOption Icon={AddIcon} addChannelOption title='Add Channels'/>
-       {channels.map(channel =>(
-        <SidebarOption title ={channel.name} id = {channel.id}/>
-      ))}
-           </div>
+  const [channelNames, setChannelNames] = useState([]);
+  const [{ user }] = useStateValue();
+
+
+ useEffect(() => {
+
+  const fetchUserChannelsAndNames = async () => {
+    try {
+      const userId = user?.uid;
+
+      if (userId) {
+        const userChannels = await getUserChannels(userId);
+        setChannelNames([]);
+
+        if (userChannels.length > 0) {
+          const names = [];
+
+          for (const channelId of userChannels) {
+            const snapshot = await db.collection('rooms').doc(channelId.toString()).get();
+            const channelName = snapshot.data()?.name;
+
+            if (channelName) {
+              names.push({ id: channelId, name: channelName });
+            }
+          }
+            setChannelNames(names);
           
-     
-      )
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching user channels and names: ', error);
     }
-    
-    export default Sidebar
+  };
+
+  fetchUserChannelsAndNames();
+
+
+}, [user]);
+
+
+  return (
+    <div className='sidebar'>
+      <div className='sidebar_header'> 
+        <div className='sidebar_info'>
+          <h2>User_Name</h2>
+          <h3>
+            <FiberManualRecordIcon />  
+            {user?.displayName} 
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & Reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved Items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel Browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People and User Groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File Browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title='Add Channels' />
+
+      {channelNames.map((channel) => (
+        <SidebarOption  title={channel.name} id={channel.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
